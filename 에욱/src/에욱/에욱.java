5c546e9f3e68a367e5696c3d4a8d9c1233d3c0ee
user1 <user1@DESKTOP-7CJF4T4>
2020-04-24 14:42:08 +0900

    주거!

--- 에욱/src/에욱/에욱.java
+++ 에욱/src/에욱/에욱.java
@@ -3,11 +3,7 @@ package 에욱;
 public class 에욱 {
 
 	public static void main(String[] args) {
-		System.out.println("에욱");
-
-		System.out.println("콰욱?");
-		
-		System.out.println("어떻게 살건지 물어본다!");
+		System.out.println("주거!");
 	}
 
 }
